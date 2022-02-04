c0857612a9598f7de55522bd874a8afad7ef9202
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-02-04 18:48:51 -0500

    Added in more Package.json links and other works. Swagging out like crazy

---
+++ booklist.js
@@ -0,0 +1,12 @@
+function isValidSubsequence(array, sequence) {
+
+    let temp = [];
+    temp.push(...sequence);
+
+    console.log(temp);
+}
+
+const map = [1, 2, 3, 4, 5, 6]
+const map2 = [2, 3, 4, 5];
+
+isValidSubsequence(map, map2);
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const axios = require('axios');
+const book = document.querySelector('#book1');
 
-axios.get('')
+const image = axios.get('https://www.googleapis.com/books/v1/volumes?q=quilting');
+
+
+const dog = async () => {
+    await axios.get('https://www.googleapis.com/books/v1/volumes?q=quilting');
+}
+
+console.log(dog);
